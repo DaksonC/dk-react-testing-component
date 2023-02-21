7e4648db6d6900bab4e7df1e84360e570a912009
Dakson Cruz <daksonc@gmail.com>
2023-02-21 18:08:53 -0300

    chore(resolve bug de tipagem e organiza as pastas

--- src/dev/App.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { Button } from '../components';
-const App = () => (React.createElement("div", { className: "content" },
-    React.createElement("h1", { className: "title" }, "Testing React Component"),
-    React.createElement(Button, { onClick: () => alert('Deu bommm! 😁'), label: 'Testing', primary: true })));
-export default App;
-//# sourceMappingURL=App.js.map
